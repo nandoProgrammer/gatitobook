2155805d7424f08e27b110b3ad609c8a36c3e5b2
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2022-03-09 10:34:36 -0300

    criação do componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import  { FormBuilder, FormGroup, Validators }  from  '@angular/forms';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,17 +15,26 @@ export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.autenticacao()
+    this.createFormLogin()
   }
 
-  createFormLogin(){
-     this.formLogin = this.formBuilder.group({
-       login: ["", [Validators.required, Validators.email]],
-       password: ["", Validators.minLength(8)]
-     })
+  createFormLogin() {
+    this.formLogin = this.formBuilder.group({
+       user: ['', Validators.required],
+       password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  fLogin(campo: string){
+    return this.formLogin.get(campo)
   }
 
   autenticacao(){
+    this.authService.auth(this.formLogin.value.user, this.formLogin.value.password).subscribe(res => {
+      //redireciona para dashboard
+    }, () => {
+      alert('Usuário ou senha errados')
+    })
   }
 
 }
